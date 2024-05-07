6b475a4f1a9c463b5b1474fc07fc832067b15304
bravovo <bogdan.nyschyi@gmail.com>
2024-05-07 16:57:16 +0300

    Added suggestions, done some refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :set_suggestions
   before_action :set_feeds
   def index
   end
@@ -6,6 +7,14 @@ class HomeController < ApplicationController
   private
   def set_feeds
     @feeds = Twit.where(user: [current_user, current_user.followings].flatten).order(created_at: :desc)
+  end
+  def set_suggestions
+    @suggestions = [current_user.followers]
+    [current_user.followers, current_user.followings].flatten.uniq.each do |f|
+      @suggestions.append([f.followers, f.followings])
+    end
+    @suggestions = [@suggestions, User.all.sample(10)].flatten.uniq - [current_user.followings, current_user].flatten
+    @suggestions = @suggestions.sample(5)
   end
 
 end
